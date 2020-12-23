1812b38361d6599a99ad6891d2f9f240291d58c8
Armin Ahmadi <75361058+dh-ceo@users.noreply.github.com>
2020-12-23 14:15:02 +0330

    Initial commit

---
+++ developershub.org/Scripts/Editor/PropertyResizerEditor.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// Author  : Armin Ahmadi
+// Email   : developershub.organization@gmail.com
+// Website : www.developershub.org
+// Copyright © 2020, Developers Hub
+// All rights reserved
+//-----------------------------------------------------------------------
+
+using DevelopersHub.Unity.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PropertyResizer))] public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        PropertyResizer myScript = (PropertyResizer)target;
+        if (GUILayout.Button("Reset"))
+        {
+            myScript.ResetUi();
+        }
+    }
+}
